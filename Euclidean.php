6691ab7eb930bf61c6bdc473d4b3b8ebdfb545da
Agharabeed <47080352+Agharabeed@users.noreply.github.com>
2021-06-07 21:30:01 +0500

    Add files via upload

--- Euclidean.php
+++ Euclidean.php
@@ -1,56 +1,50 @@
 <?php
 
-namespace NlpTools\Clustering\CentroidFactories;
+namespace NlpTools\Similarity;
 
 /**
- * Computes the euclidean centroid of the provided sparse vectors
+ * This class computes the very simple euclidean distance between
+ * two vectors ( sqrt(sum((a_i-b_i)^2)) ).
  */
-class Euclidean implements CentroidFactoryInterface
+class Euclidean implements DistanceInterface
 {
     /**
-     * If the document is a collection of tokens or features transorm it to
-     * a sparse vector with frequency information.
-     *
-     * Ex.: If 'A' appears twice in the doc the dimension 'A' will have value 2
-     * in the resulting vector
-     *
-     * @param  array $doc The doc data to transform to sparse vector
-     * @return array A sparse vector representing the document to the n-dimensional euclidean space
+     * see class description
+     * @param  array $A Either a vector or a collection of tokens to be transformed to a vector
+     * @param  array $B Either a vector or a collection of tokens to be transformed to a vector
+     * @return float The euclidean distance between $A and $B
      */
-    protected function getVector(array $doc)
+    public function dist(&$A, &$B)
     {
-        if (is_int(key($doc)))
-            return array_count_values($doc);
+        if (is_int(key($A)))
+            $v1 = array_count_values($A);
         else
-            return $doc;
-    }
+            $v1 = &$A;
+        if (is_int(key($B)))
+            $v2 = array_count_values($B);
+        else
+            $v2 = &$B;
 
-    /**
-     * Compute the mean value for each dimension.
-     *
-     * @param  array $docs   The docs from which the centroid will be computed
-     * @param  array $choose The indexes from which the centroid will be computed (if empty all the docs will be used)
-     * @return mixed The centroid. It could be any form of data a number, a vector (it will be the same as the data provided in docs)
-     */
-    public function getCentroid(array &$docs, array $choose=array())
-    {
-        $v = array();
-        if (empty($choose))
-            $choose = range(0,count($docs)-1);
-        $cnt = count($choose);
-        foreach ($choose as $idx) {
-            $doc = $this->getVector($docs[$idx]);
-            foreach ($doc as $k=>$w) {
-                if (!isset($v[$k]))
-                    $v[$k] = $w;
-                else
-                    $v[$k] += $w;
-            }
+        $r = array();
+        foreach ($v1 as $k=>$v) {
+            $r[$k] = $v;
         }
-        foreach ($v as &$w) {
-            $w /= $cnt;
+        foreach ($v2 as $k=>$v) {
+            if (isset($r[$k]))
+                $r[$k] -= $v;
+            else
+                $r[$k] = $v;
         }
 
-        return $v;
+        return sqrt(
+            array_sum(
+                array_map(
+                    function ($x) {
+                        return $x*$x;
+                    },
+                    $r
+                )
+            )
+        );
     }
 }
